16f7f5e7a18e865e123f24b82d9ec8459d60bd88
Lights_Jiao <bhjnk945766721@qq.com>
2016-09-17 15:52:19 +0800

    compelete the API desinge and union test func

---
+++ leptjson.c
@@ -0,0 +1,13 @@
+#include "leptjson.h"
+
+//parse the JSON
+int lept_parse(lept_value* v, const char* json)
+{
+	return 0;
+}
+
+//get the json type
+lept_type lept_get_type(const lept_value* v)
+{
+	return LEPT_ARRAY;
+}
