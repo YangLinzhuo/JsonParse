88ea925760247503a1a3945b6b1ee9120c40876b
Lights_Jiao <bhjnk945766721@qq.com>
2016-09-19 21:59:44 +0800

    1、合并了true/false/null 的判断函数但感觉十分的冗余,应该有更好的办法; 2、添加了一些边界值数字的单元测试; 3、判断Not_singular_value 时返回LEPT_NULL

--- leptjson.c
+++ leptjson.c
@@ -23,50 +23,49 @@ static void lept_parse_whitespace(lept_context* c)
     c->json = p;
 }
 
-
-/* null = "null" */
-static int lept_parse_null(lept_context* c, lept_value* v)
+/* value = "null" / "false" / "true" */
+static int lept_parse_literal(lept_context* c, lept_value* v, int type)
 {
-    EXPECT(c, 'n');
-    if(c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+    if (type == LEPT_NULL)
     {
-        return LEPT_PARSE_INVALID_VALUE;
+        EXPECT(c, 'n');
+        if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+        {
+            return LEPT_PARSE_INVALID_VALUE;
+        }
+
+        c->json += 3;
+        v->type = LEPT_NULL;
     }
-    
-    c->json += 3;
-    //v->type = LEPT_NULL;
+    else if (type == LEPT_TRUE)
+    {
+        EXPECT(c, 't');
+        if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        {
+            return LEPT_PARSE_INVALID_VALUE;
+        }
+
+        c->json += 3;
+        v->type = LEPT_TRUE;
+
+    }
+    else if (type == LEPT_FALSE)
+    {
+        EXPECT(c, 'f');
+        if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        {
+            return LEPT_PARSE_INVALID_VALUE;
+        }
+
+        c->json += 4;
+        v->type = LEPT_FALSE;
+    }
+
     return LEPT_PARSE_OK;
 }
 
-/* true  = "true" */
-static int lept_parse_true(lept_context* c, lept_value* v)
-{
-    EXPECT(c, 't');
-    if(c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
-    {
-        return LEPT_PARSE_INVALID_VALUE;
-    }
-    
-    c->json += 3;
-    //v->type = LEPT_TRUE;
-    return LEPT_PARSE_OK;
-}
 
-/* false = "false" */
-static int lept_parse_false(lept_context* c, lept_value* v)
-{
-    EXPECT(c, 'f');
-    if(c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
-    {
-        return LEPT_PARSE_INVALID_VALUE;
-    }
-    
-    c->json += 4;
-    //v->type = LEPT_FALSE;
-    return LEPT_PARSE_OK;
-}
-
-
+/* value = "123" */
 static int lept_parse_number(lept_context* c, lept_value* v)
 {
     char* end = NULL;
@@ -81,14 +80,14 @@ static int lept_parse_number(lept_context* c, lept_value* v)
 }
 
 
-/* value = null / false / true */
+/* value = null / false / true / number */
 static int lept_parse_value(lept_context*c, lept_value* v)
 {
     switch(*c->json)
     {
-        case 'n' : return lept_parse_null(c, v);
-        case 't' : return lept_parse_true(c, v);
-        case 'f' : return lept_parse_false(c, v);
+        case 'n' : return lept_parse_literal(c, v, LEPT_NULL);
+        case 't' : return lept_parse_literal(c, v, LEPT_TRUE);
+        case 'f' : return lept_parse_literal(c, v, LEPT_FALSE);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default  : return lept_parse_number(c, v);
     }
@@ -112,6 +111,7 @@ int lept_parse(lept_value* v, const char* json)
 		lept_parse_whitespace(&c);
 		if (*c.json != '\0')
 		{
+            v->type = LEPT_NULL;
 			ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
 		}
 	}
